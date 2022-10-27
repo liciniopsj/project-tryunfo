483c10c8570512bc0bc3a573455586e267ca4bc1
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-10-27 19:57:56 -0300

    Requisite 8

--- src/App.js
+++ src/App.js
@@ -102,6 +102,7 @@ class App extends React.Component {
       cardTrunfo,
       hasTrunfo,
       isSaveButtonDisabled,
+      savedCards,
     } = this.state;
     return (
       <div>
@@ -131,6 +132,18 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
         />
+
+        {savedCards.map((card) => (<Card
+          key={ card.cardName }
+          cardName={ card.cardName }
+          cardDescription={ card.cardDescription }
+          cardAttr1={ card.cardAttr1 }
+          cardAttr2={ card.cardAttr2 }
+          cardAttr3={ card.cardAttr3 }
+          cardImage={ card.cardImage }
+          cardRare={ card.cardRare }
+          cardTrunfo={ card.cardTrunfo }
+        />))}
       </div>
     );
   }
